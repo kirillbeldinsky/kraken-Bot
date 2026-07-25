198cea81c0c9bbbdee258c656ce35995382bf905
kirillbeldinsky <91209577+kirillbeldinsky@users.noreply.github.com>
2026-07-25 15:57:17 -0700

    Update kraken_ohlc.py

--- kraken_ohlc.py
+++ kraken_ohlc.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-import csv
 
 class KrakenOHLC:
     BASE_URL = "https://api.kraken.com/0/public/OHLC"
@@ -47,11 +46,11 @@ class KrakenOHLC:
         df = df.asfreq(f"{interval}min")
         
         #  Сохранение CSV в той же папке, где лежит модуль 
-        df.to_csv('/home/agent/kraken-Bot/kraken_ohlc_data.csv')
+        df.to_csv("kraken_ohlc_data.csv" index=False)
        # module_dir = os.path.dirname(os.path.abspath(__file__))
        # save_path = os.path.join(module_dir, "kraken_ohlc_data.csv")
 
-        df.to_csv(save_path)
+        #df.to_csv(save_path)
 
         return df
 
