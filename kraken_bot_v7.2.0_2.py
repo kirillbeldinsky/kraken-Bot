df6b13eb2791b2e5ae9b1cbe48061f4bde1a04fc
kirillbeldinsky <91209577+kirillbeldinsky@users.noreply.github.com>
2026-07-29 14:58:22 -0700

    1111

--- kraken_bot_v7.2.0_2.py
+++ kraken_bot_v7.2.0_2.py
@@ -1029,8 +1029,8 @@ def run_bot():
                         
                             atr_pct = atr / price
                             if atr_pct < 0.00045:
-                            logging.info(f"SKIP - ATR too low ({atr_pct:.5f})")
-                            continue
+                                logging.info(f"SKIP - ATR too low ({atr_pct:.5f})")
+                                continue
                             
                         # --- ФИЛЬТР: Тренд ---
                         trend = get_trend()
