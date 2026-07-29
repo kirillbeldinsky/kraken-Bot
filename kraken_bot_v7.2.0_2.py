7c1628d67a73379bfc97eda9ddaab1eaef0c2e5e
kirillbeldinsky <91209577+kirillbeldinsky@users.noreply.github.com>
2026-07-29 16:57:18 -0700

    limit

--- kraken_bot_v7.2.0_2.py
+++ kraken_bot_v7.2.0_2.py
@@ -86,7 +86,7 @@ def send_telegram(text):
         logging.error(f"send_telegram error: {e}")
 
 # --- KRAKEN API ---
-def get_ohlc(KRAKEN_SYMBOL, TIMEFRAME, limit):
+def get_ohlc(KRAKEN_SYMBOL, TIMEFRAME):
     try:
         url = f"https://api.kraken.com/0/public/OHLC?pair={KRAKEN_SYMBOL}&interval={TIMEFRAME}"
         response = requests.get(url, timeout=10)
