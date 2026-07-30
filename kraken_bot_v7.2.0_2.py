3278b8c70b00822f4f69561d6776918beae2237d
kirillbeldinsky <91209577+kirillbeldinsky@users.noreply.github.com>
2026-07-30 14:44:58 -0700

    324

--- kraken_bot_v7.2.0_2.py
+++ kraken_bot_v7.2.0_2.py
@@ -441,7 +441,7 @@ def run_bot():
                             continue
 
                         # --- ФИЛЬТР: Низкий объем ---
-                        df = get_ohlc(KRAKEN_SYMBOL, "TIMEFRAME", TRADE_STAGE_LIMIT)
+                        df = get_ohlc(KRAKEN_SYMBOL, "1h", 100)
                         if df is None:
                             logging.error("OHLC returned None, skipping this cycle")
                             await asyncio.sleep(5)
