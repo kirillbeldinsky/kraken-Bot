cdd77021caa57d7c11212d6c820f2f03c11bd81a
kirillbeldinsky <91209577+kirillbeldinsky@users.noreply.github.com>
2026-07-30 13:09:02 -0700

    23

--- kraken_bot_v7.2.0_2.py
+++ kraken_bot_v7.2.0_2.py
@@ -92,9 +92,9 @@ def get_ohlc(KRAKEN_SYMBOL, TIMEFRAME, TRADE_STAGE_LIMIT):
         response = requests.get(url, timeout=10)
         data = response.json()
 
-        result = data.get("result", {})
-        pair_key = next(iter(result), None)
-        ohlc_data = result[pair_key][-TRADE_STAGE_LIMIT:]
+            result = data.get("result", {})
+            pair_key = next(iter(result), None)
+            ohlc_data = result[pair_key][-TRADE_STAGE_LIMIT:]
 
             closes = [float(item[4]) for item in ohlc_data]
             highs = [float(item[2]) for item in ohlc_data]
